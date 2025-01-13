fddc21e9bac77413955ea551d9b4e4d5dc60497d
Davut Simsek <davutt.simsek@gmail.com>
2025-01-13 17:42:19 +0100

    added all functionalities, finished

--- script.js
+++ script.js
@@ -13,8 +13,29 @@ const btnNew = document.querySelector(`.btn--new`);
 const btnRoll = document.querySelector(`.btn--roll`);
 const btnHold = document.querySelector(`.btn--hold`);
 
-// Declerating the functions
+let scores, currentScore, activePlayer, playing;
 
+// Declerating the functions
+// Starting Conditions function
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add(`hidden`);
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player0El.classList.add(`player--active`);
+  player1El.classList.remove(`player--active`);
+};
+init();
+//  Switch player function
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -22,16 +43,6 @@ const switchPlayer = () => {
   player0El.classList.toggle(`player--active`);
   player1El.classList.toggle(`player--active`);
 };
-
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add(`hidden`);
-
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 // Rolling dice functionality
 btnRoll.addEventListener(`click`, function () {
@@ -63,7 +74,7 @@ btnHold.addEventListener(`click`, function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       // Finish the game
       playing = false;
       diceEl.classList.add(`hidden`);
@@ -80,3 +91,6 @@ btnHold.addEventListener(`click`, function () {
     }
   }
 });
+
+// New game functionality
+btnNew.addEventListener(`click`, init);
